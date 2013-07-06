faee9fbd59f4b3509b80149c15b586a61924d1c2
Michael Krasnyk <michael.krasnyk@gmail.com>
2013-07-06 12:01:28 +0300

    Init

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    console.log("request " + request);
+    var data = fs.readFileSync('index.html', 'utf8');
+    console.log("data " + data);
+    response.send(data);
+
 });
 
 var port = process.env.PORT || 5000;
